e1ee1fbbe54cd298dd37a5975560ce00951c691b
mohammad <mohammadsalimi04@gmail.com>
2025-05-26 13:03:27 +0330

    load routes and migrations to providers

--- modules/Blog/Providers/BlogServiceProvider.php
+++ modules/Blog/Providers/BlogServiceProvider.php
@@ -9,6 +9,7 @@ public function register(){
 
 }   
 Public function boot(){
-    
+    $this->loadRoutesFrom(__DIR__.'/../Routes/blog_api_routes.php');
+    $this->loadMigrationsFrom(__DIR__.'/../Database/Migrations');
 } 
 }
